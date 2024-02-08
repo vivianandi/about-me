ee480f8728a30d8b6ff7c8be1787511afc43e6d5
Vivian Davila <viviananddav@gmail.com>
2024-02-08 13:25:46 -0800

    Edits to refactor JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
 /*
-  Function Name: yourName
-  Parameters: none
-  Output: sentence with name and string
-  Return: none
-*/
-
-/*
 Original JS
 <script>
 let name = prompt("What's your name?");
@@ -13,17 +6,17 @@ document.write("Hi, " + name + ", I hope you like my page!")
 </script>
 */
 
+/*
+  Function Name: yourName
+  Parameters: none
+  Output: sentence with name and string
+  Return: none
+*/
+
 function yourName() {
   let name = prompt("What's your name?");
   document.write("Hi, " + name + ", I hope you like my page!")
 }
-
-/*
-  Function Name: catsOrDogs
-  Parameters: none
-  Output: none
-  Return: string about cats or dogs
-*/
 
 /*
 Original JS
@@ -39,15 +32,47 @@ let pet = prompt("Do you prefer cats or dogs? Enter 'cats' or 'dogs'.")
 </script>
 */
 
-function catsOrDogs() {
+/*
+  Function Name: catsOrDogs
+  Parameters: none
+  Output: string about cats or dogs
+  Return: none
+*/
 
+function catsOrDogs() {
+  let pet = prompt("Do you prefer cats or dogs? Enter 'cats' or 'dogs'.")
+  if (pet.toLowerCase() === "cats") {
+    document.write("Cats are wonderful companions!");
+  } else if (pet.toLowerCase() === "dogs") {
+    document.write("Dogs are loyal friends!");
+  } else {
+    document.write("Invalid choice. Please enter either 'cats' or 'dogs'.");
+  }
 }
 
-function doYouLikeCats() {
-  let userLikesCats = confirm("Would you like a cat?");
-  if (userLikesCats) {
-    return "Why do you like cats?";
+/*
+Original JS
+<script>
+let family = confirm("Do you love your family?");
+  if (family === true) {
+    document.write("Great, me too! Meet my family below.");
   } else {
-    return "Smart!";
+    document.write("You psychopath!");
+  }
+*/
+
+/*
+  Function Name: family
+  Parameters: none
+  Output: string 
+  Return: none
+*/
+
+function family() {
+  let family = confirm("Do you love your family?");
+  if (family === true) {
+    document.write("Great, me too! Meet my family below.");
+  } else {
+    document.write("You psychopath!");
   }
 }
